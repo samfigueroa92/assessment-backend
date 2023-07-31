90454226888852e9feb2c1641007e875ce18dc53
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2023-07-30 21:43:57 -0400

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const data = require('./data/data.json');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(200).json({ data: 'Service is running!'});
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
